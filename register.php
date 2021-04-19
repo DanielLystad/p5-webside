8a9d8111871302785e9216511ac34767178169b1
DanielLystad <82570981+DanielLystad@users.noreply.github.com>
2021-04-19 09:55:33 +0200

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'phpmyadmin';
-$DATABASE_PASS = 'Geraltofrivia123';
+$DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
